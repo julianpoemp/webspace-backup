5f640fc49707f9c61faccb0bea80ac85c6f09aa6
julianpoemp <j.poemp@campus.lmu.de>
2019-11-01 11:08:33 +0100

    improved build script

--- build.sh
+++ build.sh
@@ -11,7 +11,22 @@ echo "remove old files..."
 rm -rf prod && mkdir prod
 
 echo "build binaries..."
-pkg -t "$targets" -o prod/webspace-backup dist/app.js
+pkg -t "$targets" -o prod/webspace-backup dist/environments/app.prod.js
 
 echo "Copy assets"
+mkdir prod/webspace-backup-beta1-x64-win
+mkdir prod/webspace-backup-beta1-x64-linux
+mkdir prod/webspace-backup-beta1-x64-macos
+
+mv prod/webspace-backup-linux prod/webspace-backup-beta1-x64-linux/webspace-backup
+mv prod/webspace-backup-win.exe prod/webspace-backup-beta1-x64-win/webspace-backup.exe
+mv prod/webspace-backup-macos prod/webspace-backup-beta1-x64-macos/webspace-backup
+
+cp src/config_sample.json prod/webspace-backup-beta1-x64-win/config.json
+cp src/config_sample.json prod/webspace-backup-beta1-x64-linux/config.json
+cp src/config_sample.json prod/webspace-backup-beta1-x64-macos/config.json
+
+zip -r -X prod/webspace-backup-beta1-x64-win.zip prod/webspace-backup-beta1-x64-win
+zip -r -X prod/webspace-backup-beta1-x64-macos.zip prod/webspace-backup-beta1-x64-macos
+zip -r -X prod/webspace-backup-beta1-x64-linux.zip prod/webspace-backup-beta1-x64-linux
 echo "Build finished"
